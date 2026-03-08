f650ed4d6b0b580cc511cfa65aad7fdf1f9dd1da
jejnrko-a11y <jejnrko@gmail.com>
2026-03-08 16:25:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ else:
     if st.session_state.page == "메인 홈":
         now = get_kst()
         st.title(f"👋 {grade_str}학년 {cls_str}반 {user['name']}님")
-        st.write(f"📅 {now.year}년 {now.month}월 {now.day}일      **현재시간 : {now.strftime('%H시 %M분')}**")
+        st.write(f"📅 {now.year}년 {now.month}월 {now.day}일 &nbsp **현재시간 : {now.strftime('%H시 %M분')}**")
         
         st.markdown("### 🚀 바로가기")
         # ... 이하 동일
